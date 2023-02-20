75a55dcbe900aa753e68a8f386e822a102d04317
oluwabunmi2 <o.adebanjo@alustudent.com>
2023-02-19 17:05:03 -0800

    setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- a script that prepares a MySQL server for the projec
+-- priviledges on the database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
