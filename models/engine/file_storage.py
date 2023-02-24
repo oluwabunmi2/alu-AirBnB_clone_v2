ffc41de5fdc94beeda950ca375be41e3219648e3
Nino-Of-Tech <nwachukwunino15@gmail.com>
2023-02-24 23:55:10 +0200

    new edits

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,7 @@ import json
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-     __file_path = 'file.json'
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
