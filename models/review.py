7bad183492f1f158168e14a964810792c7517cd0
oluwabunmi2 <106445167+oluwabunmi2@users.noreply.github.com>
2023-02-24 23:12:06 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from os import getenv
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
+from sqlalchemy import Column, String, Integer, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
-    if storage_type == 'db':
-        __tablename__ = "reviews"
-        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    """Review class to store review information
+    Args:
+        BaseModel (_type_): _description_
+        Base (_type_): _description_
+    """
+    __tablename__ = "reviews"
 
-    def __init__(self, *args, **kwargs):
-        """initializes city"""
-        super().__init__(*args, **kwargs)
+    place_id = Column(ForeignKey('places.id'), nullable=False)
+    user_id = Column(ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
