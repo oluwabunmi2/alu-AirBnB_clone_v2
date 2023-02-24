46864bc0054cf8d8904d9c6836855037d730dd55
oluwabunmi2 <106445167+oluwabunmi2@users.noreply.github.com>
2023-02-24 23:08:37 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-# from models.place import Place
-from os import getenv
-# from models import storage_type
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
+# import sqlalchemy modules
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    if storage_type == 'db':
-        __tablename__ = 'cities'
-        state_id = Column(String(60),
-            ForeignKey('states.id'),
-            nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship('Place', backref='cities',
-                cascade='all, delete-orphan')
-    else:
-        state_id = ""
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes city"""
-        super().__init__(*args, **kwargs)
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete', backref="cities")
