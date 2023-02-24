2d88a611969067f895a012ed73f662e94958b3d2
oluwabunmi2 <106445167+oluwabunmi2@users.noreply.github.com>
2023-02-24 23:26:11 +0100

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,7 +5,7 @@ from models.user import User
 
 
 class test_User(test_basemodel):
-    """ test for user """
+    """ """
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -16,23 +16,19 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
-        new.first_name = "Dylan"
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
-        new.last_name = "Dylan"
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
         new = self.value()
-        new.email = "o.adebanjo@alustudent.com"
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
-        new.password = "passwordpwd"
         self.assertEqual(type(new.password), str)
