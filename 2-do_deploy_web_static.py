9f89efdcf6449c9595ed352bba231820d767724d
oluwabunmi2 <o.adebanjo@alustudent.com>
2023-03-04 15:35:04 -0800

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,45 +1,46 @@
 #!/usr/bin/python3
 """ Fabric script that distribute an archive to your web servers"""
-
 from fabric.api import local, run, env, put, sudo
 from datetime import datetime
 from os import path
-env.hosts = ['54.157.32.137', '52.55.249.213']
+env.hosts = ['35.196.49.136', '107.23.126.192']
 
 
 def do_pack():
-        """ Generate a tar archives """
-        date_recent = datetime.now().strftime("%Y%m%d%H%M%S")
-        path_ruth = "versions/web_static_{}.tgz".format(date_recent)
-        try:
-                local("mkdir -p versions")
-                local("tar -czvf {} web_static".format(path_ruth))
-                return path_ruth
-        except:
-                return None
+    """
+    function to pack in .tgz
+    """
+    local("mkdir -p versions")
+    now = datetime.today()
+    try:
+        file_name = "web_static_{}{}{}{}{}{}.tgz".format(now.year, now.month,
+                                                         now.day, now.hour,
+                                                         now.minute,
+                                                         now.second)
+        local("tar -cvzf versions/{} web_static".format(file_name))
+        return (file_name)
+    except:
+        return (None)
 
 
 def do_deploy(archive_path):
-        try:
-                if not os.path.exists(archive_path):
-                        return False
-                put(archive_path, "/tmp/")
-                fileComp = archive_path.split("/")[1].split(".")[0]
-                path = "/data/web_static/releases/{}".format(fileComp)
-                tgzFile = fileComp + '.tgz'
-                print(fileComp)
-                print(path)
-                print(tgzFile)
-                
-                run("mkdir -p {}".format(path))
-                run("tar -xvzf /tmp/{}.tgz -C {}".format(fileComp, path))
-                run("sudo rm /tmp/{}.tgz".format(fileComp))
-                run("sudo rm /data/web_static/current")
-                run("sudo ln -sf /data/web_static/releases/{}\
-                /data/web_static/current".format(fileComp))
-                run("sudo mv /data/web_static/releases/{}/web_static/* \
-                /data/web_static/releases/{}/".format(fileComp, fileComp))
-                run("rm -rf /data/web_static/releases/{}/web_static".format(fileComp))
-                return True
-        except:
-                return False
+    """
+    logic to deploy into ssh servers
+    """
+    if not path.isfile(archive_path):
+        return False
+    try:
+        put(archive_path, "/tmp/")
+        directory_path = archive_path.split(".")[0]
+        directory_path = directory_path.split("/")[-1]
+        archive_path = archive_path.split("/")[-1]
+        sudo("mkdir -p /data/web_static/releases/{}/".format(directory_path))
+        full_path = "/data/web_static/releases/{}".format(directory_path)
+        sudo("tar -xvzf /tmp/{} -C {}".format(archive_path, full_path))
+        sudo("rm -rf /tmp/{}".format(archive_path))
+        sudo("mv -f {}/web_static/* {}".format(full_path, full_path))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -sf {} /data/web_static/current".format(full_path))
+        return(True)
+    except:
+        return(False)
