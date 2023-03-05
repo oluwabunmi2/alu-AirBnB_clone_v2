c60c6ecab5a353d8314823f6d50113c937e13198
oluwabunmi2 <106445167+oluwabunmi2@users.noreply.github.com>
2023-03-05 01:49:37 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 from fabric.api import local, run, env, put, sudo
 from datetime import datetime
 from os import path
-env.hosts = ['18.234.174.125', '54.82.6.73']
+env.hosts = ['54.234.189.77', '18.234.174.125']
 
 
 def do_pack():
