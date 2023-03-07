3ae8cc487f7cca48a5df5d74f584fa023e360d30
oluwabunmi2 <106445167+oluwabunmi2@users.noreply.github.com>
2023-03-07 02:59:18 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ echo "<html>
   <head>
   </head>
   <body>
-    Best School
+     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
