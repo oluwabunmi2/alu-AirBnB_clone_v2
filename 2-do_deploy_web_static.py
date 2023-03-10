dfa64b27a12bb8eb12bd5b566895ea2126be0715
oluwabunmi2 <106445167+oluwabunmi2@users.noreply.github.com>
2023-03-10 01:29:34 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,43 @@
 #!/usr/bin/python3
-""" Fabric script that distribute an archive to your web servers"""
-from fabric.api import local, run, env, put, sudo
+"""
+Fabric script that distributes an archive to your web servers
+"""
+
 from datetime import datetime
-from os import path
-env.hosts = ['54.157.32.137', '52.55.249.213']
+from fabric.api import *
+import shlex
+import os
 
 
-def do_pack():
-    """
-    function to pack in .tgz
-    """
-    local("mkdir -p versions")
-    now = datetime.today()
-    try:
-        file_name = "web_static_{}{}{}{}{}{}.tgz".format(now.year, now.month,
-                                                         now.day, now.hour,
-                                                         now.minute,
-                                                         now.second)
-        local("tar -cvzf versions/{} web_static".format(file_name))
-        return (file_name)
-    except:
-        return (None)
+env.hosts = ['3.90.180.246', '54.84.61.133']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """
-    logic to deploy into ssh servers
-    """
-    if not path.isfile(archive_path):
+    """ Deploys """
+    if not os.path.exists(archive_path):
         return False
     try:
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
         put(archive_path, "/tmp/")
-        directory_path = archive_path.split(".")[0]
-        directory_path = directory_path.split("/")[-1]
-        archive_path = archive_path.split("/")[-1]
-        sudo("mkdir -p /data/web_static/releases/{}/".format(directory_path))
-        full_path = "/data/web_static/releases/{}".format(directory_path)
-        sudo("tar -xvzf /tmp/{} -C {}".format(archive_path, full_path))
-        sudo("rm -rf /tmp/{}".format(archive_path))
-        sudo("mv -f {}/web_static/* {}".format(full_path, full_path))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -sf {} /data/web_static/current".format(full_path))
-        return(True)
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
     except:
-        return(False)
+        return False
